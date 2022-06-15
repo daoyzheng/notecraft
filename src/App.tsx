00d9ea4eb392ddb38132e50598d6240959c1c995
Dao Zheng <daoyzheng@gmail.com>
2022-06-14 19:49:51 -0600

    Add react router; add home layout and note list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { useCallback, useState } from "react"
-import Editor from "./components/Editor"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import HomeLayout from "./layouts/HomeLayout"
+import Error from "./pages/error/Error"
 
 function App() {
-  const [doc, setDoc] = useState<string>('# hello world')
+  // const [doc, setDoc] = useState<string>('# hello world')
 
-  const handleDocChange = useCallback((newDoc: string) => {
-    setDoc(newDoc)
-  }, [])
+  // const handleDocChange = useCallback((newDoc: string) => {
+  //   setDoc(newDoc)
+  // }, [])
   return (
-    <div>
-      Home
-      <Editor onChange={handleDocChange} initialDoc={doc}/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeLayout/>}/>
+        {/* <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/> */}
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,11 @@
+import NoteList from "../pages/home/noteList/NoteList"
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <NoteList/>
+    </div>
+  )
+}
+
+export default HomeLayout
