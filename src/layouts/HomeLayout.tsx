10d2c11beabfd4eb20f9e94c134768d7cfd26ab3
Dao Zheng <daoyzheng@gmail.com>
2022-06-14 20:37:37 -0600

    Add tailwind css

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -4,6 +4,9 @@ import { Outlet } from "react-router-dom"
 const HomeLayout: React.FC = () => {
   return (
     <div>
+      <header>
+        Hello
+      </header>
       <main>
         <Outlet />
       </main>
