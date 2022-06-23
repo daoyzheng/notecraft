9434a1cbae7477a84b3a8f28c123c5441b4a796a
Dao Zheng <daoyzheng@gmail.com>
2022-06-22 23:03:49 -0600

    style admin menu

--- src/components/noteDetails/NoteDetails.tsx
+++ src/components/noteDetails/NoteDetails.tsx
@@ -5,7 +5,7 @@ const NoteDetails = ({ className } : Props) => {
   return (
     <div className={`px-2 pt-2 bg-stone-800 text-white ${className}`}>
       <div className="flex flex-row gap-x-3 items-center">
-        <div>Note Title</div>
+        <div className="text-xl">Note Title</div>
         <i className="material-icons-outlined text-sm cursor-pointer">edit</i>
       </div>
       <div className="text-xs mt-2 flex flex-row items-center gap-x-2">
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -3,8 +3,22 @@ import { Outlet } from "react-router-dom"
 const HomeLayout: React.FC = () => {
   return (
     <div className="grid grid-cols-12 h-screen w-screen">
-      <header className="bg-red-300 col-span-2">
-        Header
+      <header className="bg-zinc-900 col-span-2 text-white pt-4 px-2 justify-between flex flex-col">
+        <div>
+          <div>Notebooks</div>
+          <div className="ml-4 space-y-1 mt-1">
+            <div>NoteBook1</div>
+            <div>NoteBook2</div>
+            <div>NoteBook3</div>
+            <div>NoteBook4</div>
+            <div>NoteBook5</div>
+            <div>NoteBook6</div>
+            <div>NoteBook7</div>
+          </div>
+        </div>
+        <div>
+          Dao Zheng
+        </div>
       </header>
       <main className="col-span-10">
         <Outlet />
