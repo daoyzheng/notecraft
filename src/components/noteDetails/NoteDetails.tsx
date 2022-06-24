ae01e8fc4d0c9ead9482b08c2ccdab31ce7a2770
Dao Zheng <daoyzheng@gmail.com>
2022-06-23 21:07:12 -0600

    Add edit mode toggle

--- src/components/noteDetails/NoteDetails.tsx
+++ src/components/noteDetails/NoteDetails.tsx
@@ -1,12 +1,26 @@
+import { useCallback, useState } from "react"
+import Editor from "../editor/Editor"
+
 interface Props {
   className?: string
 }
+
 const NoteDetails = ({ className } : Props) => {
+  const [isEditMode, setIsEditMode] = useState<boolean>(false)
+  const [doc, setDoc] = useState<string>('# hello world')
+
+  const handleDocChange = useCallback((newDoc: string) => {
+    setDoc(newDoc)
+  }, [])
+
   return (
     <div className={`px-2 pt-2 bg-zinc-800 text-white ${className}`}>
-      <div className="flex flex-row gap-x-3 items-center">
+      <div className="flex flex-row gap-x-10 items-center">
         <div className="text-xl">Note Title</div>
-        <i className="material-icons-outlined text-sm cursor-pointer">edit</i>
+        { !isEditMode ?
+            <i className="material-icons-outlined text-md cursor-pointer" onClick={() => setIsEditMode(true)}>edit_note</i> :
+            <i className="material-icons-outlined text-md cursor-pointer" onClick={() => setIsEditMode(false)}>done</i>
+        }
       </div>
       <div className="text-xs mt-2 flex flex-row items-center gap-x-2">
         <div>Tag1</div>
@@ -14,7 +28,11 @@ const NoteDetails = ({ className } : Props) => {
         <div>Tag3</div>
       </div>
       <div className="mt-5">
-        Body
+        {
+          isEditMode ?
+            <Editor initialDoc={doc} onChange={handleDocChange}/> :
+            'Body'
+        }
       </div>
     </div>
   )
