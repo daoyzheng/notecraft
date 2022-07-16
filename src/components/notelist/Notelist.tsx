e6dd6a15921e9cbf48c0feaa2003543f9d3b640b
Dao Zheng <daoyzheng@gmail.com>
2022-07-16 11:23:48 -0600

    Add click outside hook

--- src/components/notelist/Notelist.tsx
+++ src/components/notelist/Notelist.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react'
+import useOutsideAlerter from '../../hooks/useOutsideAlerter'
 import { INote } from '../../interfaces/note'
 import NoteDisplay from '../noteDisplay/NoteDisplay'
 interface Props {
@@ -8,14 +10,25 @@ interface Props {
   onSelectNote?: (note: INote) => void
 }
 const Notelist = ({ className, onCreateNewNote, onSelectNote, noteList, currentNote } : Props) => {
+  const [showPopup, setShowPopup] = useState<boolean>(false)
+  const popupRef = useRef<HTMLDivElement>(null)
+  useOutsideAlerter({
+    ref: popupRef,
+    onClickOutside: handleClickOutside
+  })
   function handleCreateNewNote () {
     const newNote = {
       id: 0,
       title: 'New Note',
       date: '2020-06-14',
-      tags: []
+      tags: [],
+      isPrivate: true
     }
     onCreateNewNote && onCreateNewNote(newNote)
+  }
+
+  function handleClickOutside () {
+    setShowPopup(false)
   }
 
   function handleSelectNote (note: INote) {
@@ -23,11 +36,30 @@ const Notelist = ({ className, onCreateNewNote, onSelectNote, noteList, currentN
       onSelectNote && onSelectNote(note)
   }
 
+  const popup = () => (
+    <div ref={popupRef} className="bg-white rounded absolute text-black p-2 right-0">
+      <input
+        className="focus:outline-none"
+        autoFocus
+        placeholder="Title"
+      />
+      <div>Make note private</div>
+      <div className="flex justify-end">
+        <button>Create Note</button>
+      </div>
+    </div>
+  )
+
   return (
     <div className={`px-2 pt-1 bg-zinc-800 text-white ${className}`}>
       <div className="flex flex row items-center justify-between my-2 pb-1">
         <div className="text-lg">New Notebook</div>
-        <i className="material-icons-outlined text-sm cursor-pointer" onClick={handleCreateNewNote}>launch</i>
+        <div className="cursor-pointer relative" onClick={() => setShowPopup(true)}>
+          <i className="material-icons-outlined text-sm">launch</i>
+          {
+            showPopup && popup()
+          }
+        </div>
       </div>
       <div className="space-y-2">
         {
---
+++ src/hooks/useOutsideAlerter.tsx
@@ -0,0 +1,21 @@
+import { RefObject, useEffect, useState } from "react"
+
+interface Props {
+  ref: RefObject<Element>
+  onClickOutside: () => void
+}
+const useOutsideAlerter = ({ ref, onClickOutside } : Props) : void => {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onClickOutside()
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref])
+}
+
+export default useOutsideAlerter
