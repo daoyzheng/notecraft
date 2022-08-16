7be3df23852bda44393df0fcd81268952d38ea7f
Dao Zheng <daoyzheng@gmail.com>
2022-08-15 23:33:27 -0600

    Add notebook list component

--- src/components/globalMenu/GlobalMenu.tsx
+++ src/components/globalMenu/GlobalMenu.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react"
 import GlobalNavigationStore from "../../store/GlobalNavigationStore"
+import NotebookList from "./NotebookList"
 import useGlobalMenuKeybind from "./useGlobalMenuKeybind"
 
 interface Props {
@@ -11,21 +12,7 @@ const GlobalMenu = observer(({}: Props) => {
   })
   return (
     <>
-      <div>
-        <div className="flex justify-between items-center">
-          <div>Notebooks</div>
-          <i className="material-icons-outlined text-sm cursor-pointer">add_circle_outline</i>
-        </div>
-        <div className="ml-4 space-y-1 mt-1">
-          <div>NoteBook1</div>
-          <div>NoteBook2</div>
-          <div>NoteBook3</div>
-          <div>NoteBook4</div>
-          <div>NoteBook5</div>
-          <div>NoteBook6</div>
-          <div>NoteBook7</div>
-        </div>
-      </div>
+      <NotebookList />
       <div>
         Dao Zheng
       </div>
